ca5fd43d77e9963864c3c5ce89a6d43800cb1cf5
Kim-Onesmus <kimones1122@gmail.com>
2025-01-23 16:55:37 +0300

    initiating payment

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from intasend import APIService
 from django.conf import settings
 from django.http import JsonResponse
+import requests
 
 
 def get_intasend_service():
@@ -12,25 +13,51 @@ def get_intasend_service():
     )
 
 
+
 def initiate_payment(request):
     if request.method == 'POST':
         phone_number = request.POST.get('phone_number')
         email = request.POST.get('email')
-        amount = request.POST.get('amount')
+        amount = request.POST.get('amount')        
 
-        service = get_intasend_service()
-        response = service.collect.mpesa_stk_push(
-            phone_number=phone_number,
-            email=email,
-            amount=amount,
-            narrative="Payment for Services"
-        )
-        print('Payment initiation response', response)
-        # return JsonResponse(response)
+        url = "https://sandbox.intasend.com/api/v1/payment/mpesa-stk-push/"
+
+        payload = {
+            "amount": amount,
+            "phone_number": phone_number
+        }
+        headers = {
+            "accept": "application/json",
+            "content-type": "application/json",
+            "Authorization": f"Bearer {settings.INTASEND_API_TOKEN}"
+        }
+
+        response = requests.post(url, json=payload, headers=headers)
+        print('Response', response.status_code)
+        if response.status_code == 200:
+            response_details = response.json()
+            invoice_id = response_details.get('invoice', {}).get('invoice_id')
+            print('Invoice Id', invoice_id)
+            # print('Response data', response_details)
+            success_message = "STK push sent successfully. Please check your phone to complete the payment."
+            return render(request, 'initiate_payment.html', {'success': success_message})
+        else:
+            error_message = f"Failed to send STK push. Status:"
+            return render(request, 'initiate_payment.html', {'error': error_message})
     return render(request, 'initiate_payment.html')
 
 
+
 def check_payment_status(request):
-    service = get_intasend_service()
-    status_response = service.collect.status(invoice_id='RKML2NY')
-    return JsonResponse(status_response)
+    url = "https://sandbox.intasend.com/api/v1/payment/status/"
+
+    payload = { "invoice_id": "YVO9VZQ" }
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "Authorization": "Bearer ISSecretKey_test_96dd8981-fa1f-4ccd-841e-8aa9932ebebe"
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+
+    print(response.text)
